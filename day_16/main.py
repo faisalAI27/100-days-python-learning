ea178c2f0f86f07c5b6d6fa2485ddd3c8b46998c
faisalAI27 <u2023189@giki.edu.pk>
2026-05-31 03:30:38 +0500

    day-16 half work

---
+++ day_16/main.py
@@ -0,0 +1,20 @@
+#just an example for better understanding.
+import another_module
+print(another_module.another_variable)
+
+#first way to import the turtle module
+import turtle
+timmy=turtle.Turtle()
+print(timmy)
+
+#another way to import the turtle module
+#class is represented by capital letter.
+from turtle import Turtle,Screen
+timmy=Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+print(timmy)
+my_screen=Screen()
+my_screen.exitonclick()
