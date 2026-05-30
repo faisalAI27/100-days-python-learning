f33329d39a30a496803425b13a1752aafc95086f
faisalAI27 <u2023189@giki.edu.pk>
2026-05-31 04:19:43 +0500

    perform actions on prettytable

--- day_16/main.py
+++ day_16/main.py
@@ -1,20 +1,29 @@
 #just an example for better understanding.
-import another_module
-print(another_module.another_variable)
+#import another_module
+#print(another_module.another_variable)
 
 #first way to import the turtle module
-import turtle
-timmy=turtle.Turtle()
-print(timmy)
+#import turtle
+#timmy=turtle.Turtle()
+#print(timmy)
 
 #another way to import the turtle module
 #class is represented by capital letter.
-from turtle import Turtle,Screen
-timmy=Turtle()
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+#from turtle import Turtle,Screen
 
-print(timmy)
-my_screen=Screen()
-my_screen.exitonclick()
+#timmy=Turtle()
+#timmy.shape("turtle")
+#timmy.color("coral")
+#timmy.forward(100)
+
+#print(timmy)
+#my_screen=Screen()
+#my_screen.exitonclick()
+from prettytable import PrettyTable
+table=PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+print(table.align)
+table.align="l"
+print(table)
+print(table.align)
