07ba88476dd381af46952af3bfea3cddf2ea3a64
faisalAI27 <u2023189@giki.edu.pk>
2026-06-08 00:40:55 +0500

    drawing dashes line

--- day_18/main.py
+++ day_18/main.py
@@ -3,9 +3,12 @@ timmy_the_turtle=Turtle()
 timmy_the_turtle.shape('turtle')
 timmy_the_turtle.color('red')
 #MAKING SQUARE...
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+for _ in range(15):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
+
 
 
 
