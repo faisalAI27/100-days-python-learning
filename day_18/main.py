3e18e93ded6256191b6757acd73f7ca261c86e34
faisalAI27 <u2023189@giki.edu.pk>
2026-06-08 01:42:56 +0500

    drawing different shapes

--- day_18/main.py
+++ day_18/main.py
@@ -1,25 +1,21 @@
+import random
 from turtle import Turtle,Screen
 timmy_the_turtle=Turtle()
 timmy_the_turtle.shape('turtle')
 timmy_the_turtle.color('red')
-#MAKING SQUARE...
-for _ in range(15):
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.penup()
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.pendown()
+#MAKING DIFFERENT SHAPES.....
 
 
+colours=['red','green','blue','orange','black','brown','pink']
+def draw_shape(num_side):
+    angle=360/num_side
 
+    for _ in range(num_side):
+        angle=360/num_side
+        timmy_the_turtle.forward(130)
+        timmy_the_turtle.right(angle)
+    
 
-
-
-
-
-
-
-
-
-
-screen=Screen()
-screen.exitonclick()
+for shape_side_n in range(3,11):
+    timmy_the_turtle.color(random.choice(colours))
+    draw_shape(shape_side_n)
