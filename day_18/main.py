bccd36537c928031987cbebf990e2bad2d48054c
faisalAI27 <u2023189@giki.edu.pk>
2026-06-08 13:04:42 +0500

    random walk

--- day_18/main.py
+++ day_18/main.py
@@ -1,21 +1,11 @@
 import random
 from turtle import Turtle,Screen
 timmy_the_turtle=Turtle()
-timmy_the_turtle.shape('turtle')
-timmy_the_turtle.color('red')
-#MAKING DIFFERENT SHAPES.....
-
-
 colours=['red','green','blue','orange','black','brown','pink']
-def draw_shape(num_side):
-    angle=360/num_side
-
-    for _ in range(num_side):
-        angle=360/num_side
-        timmy_the_turtle.forward(130)
-        timmy_the_turtle.right(angle)
-    
-
-for shape_side_n in range(3,11):
+directions=[0,90,180,270]
+timmy_the_turtle.pensize(15)
+timmy_the_turtle.speed(0)
+for _ in range(200):
     timmy_the_turtle.color(random.choice(colours))
-    draw_shape(shape_side_n)
+    timmy_the_turtle.forward(30)
+    timmy_the_turtle.setheading(random.choice(directions))
