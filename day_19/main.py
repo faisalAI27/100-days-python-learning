d383a0ec059770451010f7ad6593a69d62cb3e3e
faisalAI27 <u2023189@giki.edu.pk>
2026-06-09 15:21:52 +0500

    implement etch a sketch app

--- day_19/main.py
+++ day_19/main.py
@@ -1,17 +1,32 @@
 from turtle import Turtle,Screen
 tim=Turtle()
+screen=Screen()
 
-
-
+def move_backword():
+    tim.backward(10)
 
 def move_forward():
-    tim.forward(50)
+    tim.forward(10)
+
+def counter_clock():
+    tim.left(10)
 
 
+def clock_wise():
+    tim.right(10)
+
+def clear():
+    tim.clear
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 
-screen=Screen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backword)
+screen.onkey(key="a", fun=counter_clock)
+screen.onkey(key="d", fun=clock_wise)
+screen.onkey(key="c", fun=clear)
+
 screen.listen()
-screen.onkey(key='space', fun=move_forward)
-
 screen.exitonclick()
